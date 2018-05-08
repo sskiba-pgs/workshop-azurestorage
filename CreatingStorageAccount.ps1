b6bb30130fed7a8612c694e0fc8081538ba85345
Sławek Skiba <slaweks.skiba@gmail.com>
2018-05-08 20:57:53 +0200

    Added Sending File script

--- CreatingStorageAccount.ps1
+++ CreatingStorageAccount.ps1
@@ -1,10 +1,11 @@
 ﻿$resourceGroup = "workshop-storage-rg"
+$storageaccountName = "pgsworkshowstorage"
 $location = "westeurope"
 
 New-AzureRmResourceGroup -Name $resourceGroup -Location $location
 
 New-AzureRmStorageAccount -ResourceGroupName $resourceGroup `
--Name "pgsworkshowstorage" `
+-Name $storageaccountName `
 -Location $location `
 -SkuName Standard_LRS `
 -Kind StorageV2
---
+++ SendFileToBLobContainer.ps1
@@ -0,0 +1,13 @@
+﻿$resourceGroup = "workshop-storage-rg"
+$storageaccountName = "pgsworkshowstorage"
+$containerName = "workshopblobs"
+
+$storageaccount = Get-AzureRmStorageAccount -Name $storageaccountName -ResourceGroupName $resourceGroup
+$ctx = $storageaccount.Context
+        
+New-AzureStorageContainer -Name $containerName -Context $ctx -Permission blob
+
+Set-AzureStorageBlobContent -File "C:\Workshop\kitten1.jpg" `
+  -Container $containerName `
+  -Blob "kitten001.jpg" `
+  -Context $ctx 
