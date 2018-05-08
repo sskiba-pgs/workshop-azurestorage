9de3b2f1cf289beb1cf1a5567dc95e618fc35cfa
Sławek Skiba <slaweks.skiba@gmail.com>
2018-05-08 20:01:51 +0200

    Added Creating Storage account script

---
+++ CreatingStorageAccount.ps1
@@ -0,0 +1,10 @@
+﻿$resourceGroup = "workshop-storage-rg"
+$location = "westeurope"
+
+New-AzureRmResourceGroup -Name $resourceGroup -Location $location
+
+New-AzureRmStorageAccount -ResourceGroupName $resourceGroup `
+-Name "pgsworkshowstorage" `
+-Location $location `
+-SkuName Standard_LRS `
+-Kind StorageV2
